fd0e3edbce3e5f788662840fd3a3411ef51cbc33
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-03-31 16:47:54 -0300

    Requisito 1 - Done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+const saveLoginInfo = ({ email }) => ({
+  type: 'LOGIN',
+  email,
+});
+
+export default saveLoginInfo;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import saveLoginInfo from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      buttonIsDisabled: true,
+    };
+  }
+
+  inputChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, () => this.validateForm());
+  }
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    const SIX = 6;
+    // peguei a referencia do regex nesse link https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+    const regexEmail = /\S+@\S+\.\S+/;
+    const validadeEmail = regexEmail.test(email);
+    const validadePassword = password.length >= SIX;
+    if (validadeEmail && validadePassword) {
+      this.setState({ buttonIsDisabled: false });
+    } else {
+      this.setState({ buttonIsDisabled: true });
+    }
+  }
+
+  redirect = () => {
+    const { history, loginInfo } = this.props;
+    const { email } = this.state;
+    loginInfo({ email });
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, buttonIsDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          <input
+            id="email"
+            name="email"
+            value={ email }
+            onChange={ this.inputChange }
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            id="password"
+            name="password"
+            value={ password }
+            onChange={ this.inputChange }
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ buttonIsDisabled }
+          onClick={ this.redirect }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+  loginInfo: PropTypes.func,
+}.isRequired;
+
+const mapDispatchToProps = (dispatch) => ({
+  loginInfo: (payload) => dispatch(saveLoginInfo(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
