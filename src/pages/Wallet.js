b1cdb8d58da037bfb6348fbd41e2f37ac522a137
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-04-01 16:26:59 -0300

    Requisito 5 - Done

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -19,7 +19,7 @@ class Wallet extends React.Component {
 
   render() {
     const { totalValue } = this.state;
-    const { email, isFetching } = this.props;
+    const { email, isFetching, currencies } = this.props;
     if (isFetching) {
       return (
         <div>
@@ -28,16 +28,65 @@ class Wallet extends React.Component {
       );
     }
     return (
-      <header>
-        <p data-testid="email-field">{email}</p>
-        <label data-testid="header-currency-field" htmlFor="total-field">
-          <p id="total-field" data-testid="total-field">
-            {totalValue}
-          </p>
-          BRL
-        </label>
-        <div>TrybeWallet</div>
-      </header>
+      <div>
+        <header>
+          <p data-testid="email-field">{email}</p>
+          <label data-testid="header-currency-field" htmlFor="total-field">
+            <p id="total-field" data-testid="total-field">
+              {totalValue}
+            </p>
+            BRL
+          </label>
+          <div>TrybeWallet</div>
+        </header>
+        <section>
+          <label htmlFor="value-input">
+            Valor:
+            <input
+              id="value-input"
+              name="value-input"
+              data-testid="value-input"
+              type="number"
+              min="0"
+            />
+          </label>
+          <label htmlFor="description-input">
+            Descrição:
+            <input
+              id="description-input"
+              name="description-input"
+              data-testid="description-input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="currency-input">
+            Moeda:
+            <select id="currency-input" data-testid="currency-input">
+              {currencies.map((currencie, index) => (
+                <option value={ currencie.toString() } key={ index }>
+                  { currencie }
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="method-input">
+            <select id="method-input" data-testid="method-input">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag-input">
+            <select id="tag-input" data-testid="tag-input">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </section>
+      </div>
     );
   }
 }
@@ -45,6 +94,7 @@ class Wallet extends React.Component {
 const mapStateToProps = (state) => ({
   email: state.user.email,
   isFetching: state.wallet.isFetching,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
